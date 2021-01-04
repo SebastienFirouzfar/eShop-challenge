3413b2ba23407e23b536888fa04326cf11bef727
Firouzfar <firouzfar.sebastien@gmail.com>
2021-01-04 16:57:02 +0100

    First challenge nodeJs

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const public = path.join(__dirname, 'public')
+
+//accèdes au style css
+app.use(express.static(public));
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get('/',(req, res) => {
+    res.render('index.ejs')
+;});
+
+app.listen(3000, () => console.log('Server is running'))
